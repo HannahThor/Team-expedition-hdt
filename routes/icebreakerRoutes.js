efdd351880809f143fde83c5b44bc8eebc0ff3ad
Hannah Thorley <hannahthorley2014@gmail.com>
2023-11-02 22:19:38 +0000

    everything else

--- routes/icebreakerRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as icebreaker from "../controllers/icebreakerController.js";
-
-export const icebreakerRoutes = express.Router();
-
-//icebreakerRoutes.get("/", function (req, res));
-
-icebreakerRoutes.get("/conversation_starter", icebreaker.getIcebreaker);
